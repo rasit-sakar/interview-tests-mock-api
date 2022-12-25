2d105aa464700ab6cfa1f474acc1b9d40d75bae0
Raşit Şakar <rasitsakar94@gmail.com>
2022-12-26 00:40:15 +0300

    refactor: env names fixed

--- src/configuration/app.config.ts
+++ src/configuration/app.config.ts
@@ -12,10 +12,10 @@ export interface AppConfig {
 
 export const appConfig = registerAs('app', () => ({
     name: process.env.APP_NAME || 'KrakenFlex Task',
-    description: process.env.DESCRIPTION || 'Raşit Şakar Krakenflex Task API',
+    description: process.env.APP_DESCRIPTION || 'Raşit Şakar Krakenflex Task API',
     version: process.env.APP_VERSION || 'v1',
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
-    globalUrlPrefix: process.env.GLOBAL_URL_PREFIX || 'interview-tests-mock-api',
-    documentationPath: process.env.DOCUMENTATION_PATH || 'swagger',
+    globalUrlPrefix: process.env.API_GLOBAL_URL_PREFIX || 'interview-tests-mock-api',
+    documentationPath: process.env.API_DOCUMENTATION_PATH || 'swagger',
 }));
